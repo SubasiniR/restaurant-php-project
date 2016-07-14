c3214c63db0d916c2f34a7730b426f632f0e5c85
SubasiniR <subasiniit@gmail.com>
2016-07-14 16:06:13 -0400

    Added about content

--- src/about.php
+++ src/about.php
@@ -3,7 +3,8 @@ $pageTitle = "About Us";
 include("includes/header.php"); ?>
 
 	<main>
-		<h3>About</h3>
+		<h3>About - Dosa Palace</h3>
+		<p>Dosa Palace is the perfect place for exploring the fine taste of various South Indian delicacies.</p>
 	</main>
 	
 <?php include("includes/footer.php"); ?>
