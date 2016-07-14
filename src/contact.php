7777ba2c2c4d846f036f62787c82a921253fb550
SubasiniR <subasiniit@gmail.com>
2016-07-14 16:06:54 -0400

    Added contacts page content

--- src/contact.php
+++ src/contact.php
@@ -4,6 +4,13 @@ include("includes/header.php"); ?>
 
 	<main>
 		<h3>Contact</h3>
+		<p>
+			<pre>
+				Contact us at 502-***-**** 
+						(or) 
+				E-Mail us at dosapalace@example.com
+			</pre>
+		</p>
 	</main>
 	
 <?php include("includes/footer.php"); ?>
